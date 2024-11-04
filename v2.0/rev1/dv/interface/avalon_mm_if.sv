00ac77802bef0379f20ac7c773075525b111dcec
Gabriel Iervolino <gabrieliervolinof@gmail.com>
2024-11-03 21:24:23 -0300

    Another fix

--- v2.0/rev1/dv/interface/avalon_mm_if.sv
+++ v2.0/rev1/dv/interface/avalon_mm_if.sv
@@ -15,34 +15,12 @@ interface avalon_mm_if #(parameter AVMM_WIDTH = 32, BYTE_WIDTH = 4) (
     logic [AVMM_WIDTH-1:0]       readdata;
     logic                        readdatavalid;
     logic                        waitrequest;
-
-
-    task cfg_write (
-       input [16:0] addr,
-       input [BYTE_WIDTH-1:0] be,
-       input [AVMM_WIDTH-1:0] wdata);
-
-       begin
-           @(posedge clk);
-           write       <= 1'b1;
-           read        <= 1'b0;
-           address     <= addr;
-           byteenable  <= be;
-           writedata   <= wdata;
-           $display("%0t: WRITE_MM: address %x wdata =  %x", $time, addr, wdata);
-           @(negedge waitrequest);
-           @(posedge clk);
-           write       <= 1'b0;
-       end
-
-   endtask
-
-
+ 
    task cfg_read (
        input  [16:0] addr,
        input  [BYTE_WIDTH-1:0] be,
        output [AVMM_WIDTH-1:0] rdata);
-
+ 
        begin
            @(posedge clk);
            write       <= 1'b0;
@@ -57,7 +35,7 @@ interface avalon_mm_if #(parameter AVMM_WIDTH = 32, BYTE_WIDTH = 4) (
            rdata <= readdata;
            @(posedge clk);
            $display("%0t: READ_MM: address %x rdata =  %x", $time, addr, rdata);
-
+ 
        end
  
    endtask
