6832d0cfc0d2499d33c7e52d9f868c440dd4ed9a
Gabriel Iervolino <gabrieliervolinof@gmail.com>
2024-11-03 21:08:41 -0300

    Small interface changes.

--- v2.0/rev1/dv/interface/avalon_mm_if.sv
+++ v2.0/rev1/dv/interface/avalon_mm_if.sv
@@ -61,6 +61,29 @@ interface avalon_mm_if #(parameter AVMM_WIDTH = 32, BYTE_WIDTH = 4) (
        end
  
    endtask
-
+     function void clear();
+     begin
+         write       <= 1'b0;
+         read        <= 1'b0;
+         address     <= '0;
+         byteenable  <= '0;
+         writedata   <= '0;
+         //rst_n       <= 1'b0;
+     end
+     endfunction
+ 
+     function void cfg_write();
+     input [16:0] addr;
+     input [BYTE_WIDTH-1:0] be;
+     input [AVMM_WIDTH-1:0] wdata;
+ 
+     begin
+         write       <= 1'b1;
+         read        <= 1'b0;
+         address     <= addr;
+         byteenable  <= be;
+         writedata   <= wdata;
+     end
+     endfunction
 
 endinterface : avalon_mm_if
